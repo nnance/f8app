2bd17f0e90756f485444b3b65ac1b93a05266a5c
Art Natthaphat <nattapat.art@gmail.com>
2016-11-11 17:11:13 +0700

    add mailgun adapter

--- server/server.js
+++ server/server.js
@@ -30,11 +30,15 @@ import ParseDashboard from 'parse-dashboard';
 
 const SERVER_PORT = process.env.PORT || 8080;
 const SERVER_HOST = process.env.HOST || 'localhost';
+const APP_NAME = process.env.APP_NAME || 'clogii';
 const APP_ID = process.env.APP_ID || 'oss-f8-app-2016';
 const MASTER_KEY = process.env.MASTER_KEY || '70c6093dba5a7e55968a1c7ad3dd3e5a74ef5cac';
 const DATABASE_URI = process.env.DATABASE_URI || 'mongodb://localhost:27017/dev';
 const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production';
 const DASHBOARD_AUTH = process.env.DASHBOARD_AUTH;
+const MAILGUN_FROM_ADDRESS = process.env.MAILGUN_FROM_ADDRESS || 'admin@localhost';
+const MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN || '';
+const MAILGUN_KEY = process.env.MAILGUN_KEY || '';
 
 Parse.initialize(APP_ID);
 Parse.serverURL = `http://localhost:${SERVER_PORT}/parse`;
@@ -61,6 +65,16 @@ server.use(
     masterKey: MASTER_KEY,
     fileKey: 'f33fc1a9-9ba9-4589-95ca-9976c0d52cd5',
     serverURL: `http://${SERVER_HOST}:${SERVER_PORT}/parse`,
+    publicServerURL: `http://${SERVER_HOST}:${SERVER_PORT}/parse`,
+    appName: APP_NAME,
+    emailAdapter: {
+      module: 'parse-server-simple-mailgun-adapter',
+      options: {
+        fromAddress: MAILGUN_FROM_ADDRESS,
+        domain: MAILGUN_DOMAIN,
+        apiKey: MAILGUN_KEY
+      }
+    }
   })
 );
 
