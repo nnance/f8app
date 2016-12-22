afc9b98f2bd434716fd4c5f60afc33852dc16728
Art Nattapat <nattapat.art@gmail.com>
2016-12-22 15:27:00 +0700

    fix DashButton

--- js/login/DashButton.js
+++ js/login/DashButton.js
@@ -3,20 +3,36 @@ import {Image, View, Text, TextInput, TouchableOpacity} from 'react-native';
 import ClogiiButton from 'ClogiiButton';
 import styles from './styles';
 
+import F8Colors from 'F8Colors';
+
 export default class DashButton extends React.Component {
   render() {
     return (
-        <TouchableOpacity onPress={() => this.props.onPress && this.props.onPress()}>
+        <TouchableOpacity onPress={() => this.props.onPress && this.props.onPress()} style={{flexDirection: 'row', padding: 10}}>
+          <View style={{
+              height: 1,
+              flex: 1,
+              marginVertical: 12,
+              marginHorizontal: 15,
+              backgroundColor: F8Colors.fadedLightText
+            }}/>
           <Text
-            style={{paddingVertical: 40, color: 'white'}}
+            style={{color: 'white'}}
             >
-            ––––––––––– {this.props.caption} ––––––––––
+            {this.props.caption}
           </Text>
+          <View style={{
+              height: 1,
+              flex: 1,
+              marginVertical: 12,
+              marginHorizontal: 15,
+              backgroundColor: F8Colors.fadedLightText
+            }}/>
         </TouchableOpacity>
     );
   }
 }
 
-export const DashButtonWithContainer = (props) => (<View style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}>
+export const DashButtonWithContainer = (props) => (<View style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center', margin: 20}}>
   <DashButton {...props}/>
 </View>);
