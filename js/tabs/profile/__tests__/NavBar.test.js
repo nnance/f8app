2d242150a94bd2681238e40b3fd2dfa851b7a340
Art Nattapat <nattapat.art@gmail.com>
2016-12-26 12:26:57 +0700

    use jest-reac-native instead of react-native-mock

--- js/tabs/profile/__tests__/NavBar.test.js
+++ js/tabs/profile/__tests__/NavBar.test.js
@@ -1,18 +1,21 @@
-import 'react-native-mock/mock';
 import React from 'react';
 import {
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  TouchableHighlight
 } from 'react-native';
-
 import { shallow, mount, render } from 'enzyme';
 
 import NavBar from '../NavBar';
 
 describe('NavBar component', () => {
-  it('render backButton by default', (done) => {
-    const wrapper = shallow(<View onPress={() => done()}></View>);
+  it('render backButton by default', () => {
+    const f = jest.fn();
+    const wrapper = shallow(<A onPress={f}></A>);
     wrapper.simulate('press');
+    expect(f).toBeCalled();
   });
 });
+
+const A = props => (<TouchableOpacity onPress={props.onPress}></TouchableOpacity>)
