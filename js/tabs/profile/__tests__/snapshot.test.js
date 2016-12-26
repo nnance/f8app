48a5082c9ec6e1022a3abd5a6de8d4b9eee0983f
Art Nattapat <nattapat.art@gmail.com>
2016-12-26 16:39:01 +0700

    add snapshot test (only component not need to test functional)

---
+++ js/tabs/profile/__tests__/snapshot.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+const mockBookmark = [
+  {
+    title: "Richy Rich! รวยมากนะ! รู้ยังคะทุกคน",
+    cover: require('./img/A.png'),
+    categoryCover: require('./img/category/N.png'),
+    bookmarkCount: 7
+  },
+  {
+    title: "Money Honey คุณชายหน้าตายกับยัยขี้งก",
+    cover: require('./img/B.png'),
+    categoryCover: require('./img/category/M.png'),
+    bookmarkCount: 3
+  }
+];
+
+describe('BookmarkScreen', () => {
+  const BookmarkScreen = require('../BookmarkScreen').default;
+  it('render BookmarkScreen', () => {
+    const tree = renderer.create(<BookmarkScreen bookmark={mockBookmark}/>);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
+
+describe('CircleImageWithCategory', () => {
+  const CircleImageWithCategory = require('../CircleImageWithCategory').default;
+  it('render CircleImageWithCategory', () => {
+    const tree = renderer.create(<CircleImageWithCategory
+      source={{uri: 'x.jpg'}}
+      categorySource={{uri: 'x.jpg'}}
+      size={100}
+    />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+})
