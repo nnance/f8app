78f2c4a6c995b7e30bf11a1c654989d340808c56
Art Nattapat <nattapat.art@gmail.com>
2016-12-26 15:07:35 +0700

    add NavBar test

--- js/tabs/profile/__tests__/NavBar.test.js
+++ js/tabs/profile/__tests__/NavBar.test.js
@@ -2,19 +2,40 @@ import React from 'react';
 import {
   Text,
   View,
-  TouchableOpacity,
-  TouchableHighlight
+  TouchableOpacity
 } from 'react-native';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
 import NavBar from '../NavBar';
 
-describe('NavBar component', () => {
+describe('<NavBar/>', () => {
   it('render backButton by default', () => {
-    const f = jest.fn();
-    const wrapper = shallow(<A onPress={f}></A>);
-    wrapper.simulate('press');
-    expect(f).toBeCalled();
+    const tree = renderer.create(<NavBar/>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('render title', () => {
+    const wrapper = shallow(<NavBar title="test title"/>);
+    expect(wrapper.contains('test title')).toBe(true);
+  });
+
+  it('can custom rightMenu', () => {
+    const wrapper = shallow(<NavBar renderRightMenu={() => <Text>RButton</Text>}/>)
+    expect(wrapper.contains(<Text>RButton</Text>)).toBe(true);
+  });
+
+  it('handle onPress', () => {
+    const leftFn = jest.fn();
+    const rightFn = jest.fn();
+    const wrapper = shallow(<NavBar renderRightMenu={() => <Text>RButton</Text>} onLeftPress={leftFn} onRightPress={rightFn}/>);
+    const touchables = wrapper.find(TouchableOpacity);
+    expect(touchables.length).toBeGreaterThan(0);
+    touchables.at(0).simulate('press');
+    expect(leftFn).toBeCalled();
+    expect(rightFn).not.toBeCalled();
+    touchables.at(1).simulate('press');
+    expect(rightFn).toBeCalled();
   });
 });
 
