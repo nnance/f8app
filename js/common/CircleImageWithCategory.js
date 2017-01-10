d14152372162fbafe73a5e0a3e6eebb8579e39af
Art Nattapat <nattapat.art@gmail.com>
2017-01-10 12:13:23 +0700

    add style to CircleImage[WithCategory]

--- js/common/CircleImageWithCategory.js
+++ js/common/CircleImageWithCategory.js
@@ -7,15 +7,15 @@ import {
 } from 'react-native';
 
 export default (props) => (<View style={{width: props.size, height: props.size}}>
-  <CircleImage {...props} style={[props.style, {backgroundColor: 'transparent'}]}/>
+  <CircleImage {...props} style={[props.imageStyle, {backgroundColor: 'transparent'}]}/>
   <Image
     source={props.categorySource}
-    style={{
+    style={[{
       position: 'absolute',
       bottom: props.size / 32,
       right: props.size / 32,
       width: props.size / 4,
       height: props.size / 4
-    }}
+    }, props.categoryStyle]}
   />
 </View>);
