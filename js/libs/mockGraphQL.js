d4f643726330e835f55a68441ec5b4cf09360c83
Art Nattapat <nattapat.art@gmail.com>
2017-01-18 17:40:21 +0700

    add warning

--- js/libs/mockGraphQL.js
+++ js/libs/mockGraphQL.js
@@ -2,6 +2,10 @@ import {graphql} from 'graphql';
 import casual from 'casual';
 let MockSchema = require('../../server/cloud/graphql/mockSchema');
 
+if (process.env.NODE_ENV !== 'test') {
+  throw new Error('for test only');
+}
+
 export default function(query, options = {}) {
   casual.seed(1);
   return graphql(MockSchema, query.loc.source.body, null, {
