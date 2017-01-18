660c5c8487fdb6534b801dba24cb2eabd7cc91ce
Art Nattapat <nattapat.art@gmail.com>
2017-01-18 16:59:08 +0700

    fix styles

--- js/common/styles.js
+++ js/common/styles.js
@@ -1,7 +1,7 @@
 export const colors = {
   greyBackground: 'rgb(250, 250, 250)',
   fadedGreyBackground: 'rgba(0, 0, 0, 0.2)',
-  greyBorder: 'rgba(0, 0, 0, 0.6)',
+  greyBorder: '#eeeeee',
   pinkBorder: 'rgba(242, 215, 234, 0.8)',
   fadedWhite: 'rgba(255, 255, 255, 0.3)',
   textFadedGrey: 'rgba(0, 0, 0, 0.3)',
