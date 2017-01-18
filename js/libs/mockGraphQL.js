3fb8b375f919e31748b02703cc79634e2720ed43
Art Nattapat <nattapat.art@gmail.com>
2017-01-18 17:28:25 +0700

    set casual.seed every call mock graphql

--- js/libs/mockGraphQL.js
+++ js/libs/mockGraphQL.js
@@ -1,7 +1,9 @@
 import {graphql} from 'graphql';
+import casual from 'casual';
 let MockSchema = require('../../server/cloud/graphql/mockSchema');
 
 export default function(query, options = {}) {
+  casual.seed(1);
   return graphql(MockSchema, query.loc.source.body, null, {
     request: {
       get: jest.fn(() => 'localhost')
