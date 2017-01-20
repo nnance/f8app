0fb81cacb9bf4ec251f969a1a3a58bef3f4fbe7e
Art Nattapat <nattapat.art@gmail.com>
2017-01-20 16:53:21 +0700

    add test

--- js/tabs/shelf/__tests__/ClogCategory.js
+++ js/tabs/shelf/__tests__/ClogCategory.js
@@ -1,9 +1,13 @@
 import 'isomorphic-fetch';
 import React from 'react';
+import {Dimensions} from 'react-native';
 import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
+
 import graphql from '../../../libs/mockGraphQL';
 import ClogCategoryContainer, {query, mapQueryToProps, mapPropsToOptions} from '../containers/ClogCategory';
 import ClogCategoryComponent from '../components/ClogCategory';
+import HorizontalListView from '../../../common/HorizontalListView';
 
 describe('Shelf.ClogCategory', () => {
   it('can query schema', async () => {
@@ -19,5 +23,28 @@ describe('Shelf.ClogCategory', () => {
     const props = mapQueryToProps({data: result.data});
     const tree = renderer.create(<ClogCategoryComponent category="D" {...props} />);
     expect(tree).toMatchSnapshot();
+  });
+
+  it('navigate to ClogListView onPress viewAll', async () => {
+    const result = await graphql(query, mapPropsToOptions({category: 'D'}));
+    const props = mapQueryToProps({data: result.data});
+    const navigator = {
+      push: jest.fn()
+    };
+    const dump = shallow(<ClogCategoryComponent {...{...props, navigator, category: 'D'}}/>);
+    const wrapper = shallow(dump.instance().renderButtonViewAllClog({sortBy: 'TRENDING'}));
+    wrapper.simulate('press');
+    expect(navigator.push).toBeCalledWith({page: 'clog-list-view', category: 'D', sortBy: 'TRENDING'});
+  });
+
+  it('set currentClogBanner onScroll', async () => {
+    const windowSize = Dimensions.get('window').width - 20;
+    const result = await graphql(query, mapPropsToOptions({category: 'D'}));
+    const props = mapQueryToProps({data: result.data});
+    const wrapper = shallow(<ClogCategoryComponent {...{...props, category: 'D'}}/>);
+    wrapper.find(HorizontalListView).simulate('scroll', {nativeEvent: {contentOffset: {x: windowSize * 2}}});
+    expect(wrapper.state().currentClogBanner).toBe(2);
+    wrapper.find(HorizontalListView).simulate('scroll', {nativeEvent: {contentOffset: {x: windowSize * 0}}});
+    expect(wrapper.state().currentClogBanner).toBe(0);
   });
 });
--- js/tabs/shelf/components/Home.js
+++ js/tabs/shelf/components/Home.js
@@ -80,7 +80,7 @@ class Home extends React.Component {
   renderTrendingButton() {
     return (
       <BorderButton caption="ทั้งหมด" type="fadedWhite" containerStyle={styles.clogListButton}
-        onPress={() => this.props.navigator.push({page: 'clog-list-view', title: "ยอดนิยม"})}
+        onPress={() => this.props.navigator.push({page: 'clog-list-view', title: 'ยอดนิยม', sortBy: 'TRENDING'})}
         />
     );
   }
