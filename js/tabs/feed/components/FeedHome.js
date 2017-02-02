4530a5ded30e52790d7f09e9fb47e9c96a9ed145
Chalermrat Srinara <schalermrat@gmail.com>
2017-02-02 15:56:19 +0700

    Feed tab navigator

---
+++ js/tabs/feed/components/FeedHome.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+  RefreshControl
+} from 'react-native';
+import FixBugScrollView from '../../../common/FixBugScrollView';
+import NavBar from './NavBar';
+import FeedList from './FeedList';
+import data from '../data';
+
+
+class FeedHome extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: data,
+      isRefreshing: false
+    }
+  }
+
+  // Pull to refresh handler
+  _onRefresh = () => {
+    this.setState({isRefreshing: true})
+    setTimeout(() => {
+      console.log('Feeds loading....');
+      this.setState({isRefreshing: false})
+    }, 3000)
+  }
+
+  render() {
+    return (
+      <View style={{flex: 1, backgroundColor: '#F0F0F0'}}>
+        <NavBar
+          renderRightMenu={() => (
+              <TouchableOpacity style={{zIndex: 3}}><Image style={{height: 20, resizeMode: 'contain'}} source={require('../img/search.png')}/></TouchableOpacity>
+            )}
+          renderTitle={() => (
+              <Image style={{width: 80, height: 30, resizeMode: 'contain'}} source={require('../img/title.png')}/>
+            )}
+          titleStyle={{
+              flex: 1,
+              alignItems: 'flex-start'
+            }}
+        />
+        <FixBugScrollView
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.isRefreshing}
+              onRefresh={this._onRefresh}
+              tintColor="#ff0000"
+              title="..."
+              titleColor="#000"
+              />
+          }>
+          <View style={{backgroundColor: '#7d7d7d'}}>
+            <FeedList data={this.state.data}/>
+          </View>
+        </FixBugScrollView>
+
+      </View>
+    )
+  }
+}
+
+export default FeedHome
