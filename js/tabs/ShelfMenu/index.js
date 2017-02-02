6580b0add72412d40cccc0b425486f007a44529f
Art Nattapat <nattapat.art@gmail.com>
2017-02-02 16:23:55 +0700

    [Shelf/Menu] add onPress each button

--- js/tabs/ShelfMenu/index.js
+++ js/tabs/ShelfMenu/index.js
@@ -10,6 +10,7 @@ import {
 
 import {STATUS_BAR_HEIGHT, colors} from '../../common/styles';
 import CircleImage from '../../common/CircleImage';
+import {mapSource} from '../../common/utils';
 import {getCategoryIcon, getCategoryLogo} from '../../models/clog';
 
 const CloseMenuButton = ({onPress}) => (
@@ -31,31 +32,33 @@ const HideTagButton = ({onPress}) => (
 );
 
 const Topic = ({title, children, style}) => (
-  <View style={style}>
-    <View style={{backgroundColor: colors.fadedGreyBackground, paddingVertical: 4, paddingLeft: 10}}>
+  <View>
+    <View style={[{backgroundColor: colors.fadedGreyBackground, paddingVertical: 4, paddingLeft: 10}]}>
       <Text style={{color: 'rgba(255, 255, 255, 0.2)', fontSize: 12}}>{title}</Text>
     </View>
-    <View style={{paddingVertical: 0, paddingLeft: 10}}>
+    <View style={[{paddingVertical: 0, paddingLeft: 10}, style]}>
       {children}
     </View>
   </View>
 );
 
-const FadedMenuItem = ({title, source}) => (
-  <View style={{flexDirection: 'row', paddingVertical: 6, paddingHorizontal: 5, alignItems: 'center'}}>
+const FadedMenuItem = ({title, source, onPress}) => (
+  <TouchableOpacity onPress={onPress} style={{flexDirection: 'row', paddingVertical: 6, paddingHorizontal: 5, alignItems: 'center'}}>
     <Image source={source} style={{width: 20, height: 20, resizeMode: 'contain'}}/>
     <View style={{marginLeft: 10}}>
       <Text style={[{fontSize: 13}, styles.fadedWhiteText]}>{title}</Text>
     </View>
-  </View>
+  </TouchableOpacity>
 );
 
-const WhiteMenuItem = ({title, source, renderedButton, style}) => (
+const WhiteMenuItem = ({title, source, renderedButton, style, onPress}) => (
   <View style={[{flexDirection: 'row', paddingVertical: 10, paddingLeft: 5, alignItems: 'center'}, style]}>
-    <CircleImage source={source} size={25}/>
-    <View style={{marginLeft: 10}}>
-      <Text style={{color: 'white', fontWeight: 'bold'}}>{title}</Text>
-    </View>
+    <TouchableOpacity onPress={onPress} style={{flexDirection: 'row', alignItems: 'center'}}>
+      <CircleImage source={source} size={25}/>
+      <View style={{marginLeft: 10}}>
+        <Text style={{color: 'white', fontWeight: 'bold'}}>{title}</Text>
+      </View>
+    </TouchableOpacity>
     <View style={{flex: 1, paddingRight: 10, alignItems: 'flex-end', justifyContent: 'center'}}>
       {renderedButton}
     </View>
@@ -76,6 +79,7 @@ class ClogMenuItem extends React.Component {
         <WhiteMenuItem
           style={{paddingTop: 10, paddingBottom: this.state.showTag ? 5 : 10}}
           title={this.props.title}
+          onPress={this.props.onPress}
           source={this.props.source}
           renderedButton={!this.state.showTag ? <ShowTagButton onPress={this.onShowTagPress.bind(this)}/> : <HideTagButton onPress={this.onHideTagPress.bind(this)}/>}
           />
@@ -105,13 +109,19 @@ class ClogMenuItem extends React.Component {
     });
   }
 
+  onTagPress(tag) {
+    this.props.onTagPress && this.props.onTagPress();
+  }
+
   renderTagItem() {
-    const tags = [{title: 'Chat Clog'}, {title: 'Vide Clog'}];
+    const tags = this.props.tags;
     return (
       <View>
         {
           tags.map(tag => (
-            <Text style={[styles.fadedWhiteText, {paddingVertical: 1}]}>{tag.title}</Text>
+            <TouchableOpacity onPress={this.onTagPress.bind(this, tag.id)}>
+              <Text style={[styles.fadedWhiteText, {paddingVertical: 5}]}>{tag.title}</Text>
+            </TouchableOpacity>
           ))
         }
       </View>
@@ -121,6 +131,7 @@ class ClogMenuItem extends React.Component {
 
 class ShelfMenu extends React.Component {
   render() {
+    const mockTags = [{id: '1', title: 'Chat Clog'}, {id: '2', title: 'Video Clog'}, {id: '3', title: 'Picture Clog'}, {id: '4', title: 'Pet Clog'}]
     return (
       <View style={{flex: 1, backgroundColor: 'rgba(50, 31, 85, 1)'}}>
         <View style={{height: STATUS_BAR_HEIGHT}}/>
@@ -129,22 +140,22 @@ class ShelfMenu extends React.Component {
         </View>
         <ScrollView>
           <Topic title="หมวดหมู่เรื่องราว">
-            <ClogMenuItem title="Gag Clog" source={getCategoryLogo('G')}/>
-            <ClogMenuItem title="Diary Clog" source={getCategoryLogo('D')}/>
-            <ClogMenuItem title="Novel Clog" source={getCategoryLogo('N')}/>
-            <ClogMenuItem title="Myth Clog" notShowBottomLine={true} source={getCategoryLogo('M')}/>
+            <ClogMenuItem tags={mockTags} title="Gag Clog" source={getCategoryIcon('G')}/>
+            <ClogMenuItem tags={mockTags} title="Diary Clog" source={getCategoryIcon('D')}/>
+            <ClogMenuItem tags={mockTags} title="Novel Clog" source={getCategoryIcon('N')}/>
+            <ClogMenuItem tags={mockTags} title="Myth Clog" notShowBottomLine={true} source={getCategoryIcon('M')}/>
           </Topic>
           <Topic title="ข้อมูล">
-            <WhiteMenuItem title="David Beckham" source={getCategoryIcon('D')}/>
+            <WhiteMenuItem onPress={this.props.onProfilePress} title="David Beckham" source={getCategoryIcon('D')}/>
           </Topic>
-          <Topic title="เกี่ยวกับ">
-            <FadedMenuItem title="เกี่ยวกับ Clogii" source={require('./img/about.png')}/>
-            <FadedMenuItem title="ข้อกำหนดในการใช้ Clogii" source={require('./img/policy.png')}/>
+          <Topic title="เกี่ยวกับ" style={{paddingVertical: 5}}>
+            <FadedMenuItem onPress={this.props.onAboutClogiiPress} title="เกี่ยวกับ Clogii" source={require('./img/about.png')}/>
+            <FadedMenuItem onPress={this.props.onPolicyPress} title="ข้อกำหนดในการใช้ Clogii" source={require('./img/policy.png')}/>
           </Topic>
-          <Topic title="ติดตาม">
-            <FadedMenuItem title="Facebook" source={require('./img/fb.png')}/>
-            <FadedMenuItem title="Line" source={require('./img/line.png')}/>
-            <FadedMenuItem title="Instagram" source={require('./img/ig.png')}/>
+          <Topic title="ติดตาม" style={{paddingVertical: 5}}>
+            <FadedMenuItem onPress={this.props.onFacebookPress} title="Facebook" source={require('./img/fb.png')}/>
+            <FadedMenuItem onPress={this.props.onLinePress} title="Line" source={require('./img/line.png')}/>
+            <FadedMenuItem onPress={this.props.onInstagramPress} title="Instagram" source={require('./img/ig.png')}/>
           </Topic>
         </ScrollView>
       </View>
