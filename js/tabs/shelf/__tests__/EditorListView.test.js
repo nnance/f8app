9695c06158e4d0356af680141e1c18ca5e701e27
Art Nattapat <nattapat.art@gmail.com>
2017-02-10 15:59:07 +0700

    [EditorListView] add test

---
+++ js/tabs/shelf/__tests__/EditorListView.test.js
@@ -0,0 +1,18 @@
+import 'isomorphic-fetch';
+import React from 'react';
+
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
+import graphql from '../../../libs/mockGraphQL';
+import { query, mapQueryToProps, mapPropsToOptions } from '../containers/EditorListView';
+import EditorListView from '../components/EditorListView';
+
+describe('Shelf.EditorListView', () => {
+  it('render', async () => {
+    const result = await graphql(query, mapPropsToOptions({ category: 'D' }));
+    const props = mapQueryToProps({ data: result.data });
+    const tree = renderer.create(<EditorListView category="D" {...props}/>);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
