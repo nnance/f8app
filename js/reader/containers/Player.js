e4fdb35be945324c2ff1ad7f3254611923ba5406
Art Nattapat <nattapat.art@gmail.com>
2017-02-28 13:18:32 +0700

    fix reader/container/Player

--- js/reader/containers/Player.js
+++ js/reader/containers/Player.js
@@ -1,25 +1,42 @@
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import _ from 'lodash';
 
+import { filterClogId, filterEpisodeId, withAddEpisodeBookmark, withRemoveEpisodeBookmarks } from '../../models/bookmark';
 import Player from '../components/Player';
+import { query as BookmarkScreenQuery } from '../../tabs/profile/containers/BookmarkScreen';
+import { query as BookmarkDetailQuery } from '../../tabs/profile/containers/BookmarkDetail';
 
 export const query = gql`
   query PlayerQuery($id: MongoID!){
     episode(filter: { _id: $id }) {
-      ...Player
+      ...PlayerEpisode
+    }
+    me {
+      id
+      episodeBookmarks {
+        ...PlayerBookmark
+      }
     }
   }
   ${Player.fragments.episode}
+  ${Player.fragments.bookmark}
 `;
 
 export const mapQueryToProps = ({ data }) => {
-  const { episode, error, loading } = data;
+  const { episode, error, loading, me } = data;
   if (error) {
     console.error('graphql error: ', error);
   }
+  let episodeBookmarks;
+  if (episode && me) {
+    episodeBookmarks = filterEpisodeId(filterClogId(me.episodeBookmarks, episode.clogId), episode.id);
+  }
+
   return {
     loading,
     episode: episode || {},
+    episodeBookmark: episodeBookmarks ? episodeBookmarks[0] : null,
   };
 };
 export const mapPropsToOptions = ({ id }) => ({
@@ -28,7 +45,7 @@ export const mapPropsToOptions = ({ id }) => ({
   },
 });
 
-export default graphql(query, {
+export default withRemoveEpisodeBookmarks(withAddEpisodeBookmark(graphql(query, {
   props: mapQueryToProps,
   options: mapPropsToOptions,
-})(Player);
+})(Player)));
